23bd1cd8f7c6036645152b713647467285d02e5f
humansirmathsclub3 <humansirmathsclub.3@gmail.com>
2026-03-24 15:52:10 +0530

    Add files via upload

---
+++ app/src/main/java/com/humansir/myapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.humansir.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://script.google.com/macros/s/AKfycbyjeN_3GMou0x45M1nC_d5IALv1kc8V_W7tZtF57OllKuJoWfiCfaBrIDHzszvbCdMd8w/exec");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
